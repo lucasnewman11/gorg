4e151d17b0218a2341d6012cc0ce129273e26d78
Your Name <you@example.com>
2017-04-21 15:04:39 -0400

    routine commit and push

--- Control/Commands/Basic.py
+++ Control/Commands/Basic.py
@@ -1,5 +1,17 @@
-###### BASE CLASSES ######
+class Excursion():
+    "Records important information about a gate at the outset of a command or command sequence, so that aspects of state can be easily resume after."
+    def __init__(self, gate):
+        self._gate = gate
+        self._gate_snap = {}
+        self.snapshot()
 
+    def snapshot(self):
+        self._gate_snap["keymap"] = self._gate.keymap()
+
+    def resolve(self):
+        self._gate.set_keymap(self._gate_snap["keymap"])
+
+###### BASE CLASSES ######
 class Command():
     # Base interface for commands.  Commands are simply class definitions - they are not instantiated.
 
@@ -15,36 +27,61 @@ class Command():
 
     def neutral():
         raise NotImplementedError
-        
-    def execute(fie, config):
+
+    def body(fie, config):
         raise NotImplementedError
 
-    def _message(fie, message):
-        gate = Gate()
-        inter = Interface
-        
+    @classmethod
+    def execute(cls, fie, config):
+        print(cls.name())
+        print(cls.excursion())
+        print(config.excursions)
+        if cls.excursion() == "START":
+            config.excursions[fie.gate] = Excursion(fie.gate)
+        elif cls.excursion() == "END":
+            config.excursions[fie.gate].resolve()
+        cls.body(fie, config)
+            
+###### WRITE vs. NEUTRAL ######
 
 class WriteCommand(Command):
-    # Base interface for commands that modify the fragments of their target gate.
+    "Base interface for commands that modify the fragments of their target gate."
     def neutral():
         return False
 
 class NeutralCommand(Command):
-    # Base interface for commands that do not modify the fragments of their target gate
-
+    "Base interface for commands that do not modify the fragments of their target gate."
     def neutral():
         return True
 
+###### EXCURSION BEGINNINGS AND ENDINGS ######
+
+class ExcursionStart():
+    "To be inherited by commands that initiate an extended experience, thereby triggering an excursion."
+    def excursion():
+        return "START"
+
+class ExcursionEnd():
+    "To be inherited by commands that concluce and extended experience, thereby triggering resolution of an excursion."
+    def excursion():
+        return "END"
+
+class NoExcursion():
+    "To be inherited by commands that neither begin nor end an excurison."
+    def excursion():
+        return False
+
+    
 ###### BASIC MAP ######    
 
-class Basic_Map(NeutralCommand):
+class Basic_Map(NeutralCommand, NoExcursion):
     "Sets the active keymap of the calling gate to 'Basic'"
-    def execute(fie, config):
-        fie.gate.set_active_map(fie.commander.blueprints()["Keymaps"]["Basic"].materialize())
+    def body(fie, config):
+        fie.gate.set_keymap(fie.commander.blueprints()["Keymaps"]["Basic"].materialize())
 
 ###### BASIC TEXT MANIPULATION ######
 
-class TextInsertionCommand(WriteCommand):
+class TextInsertionCommand(WriteCommand, NoExcursion):
     "Base interface for commands that insert text into the target gate at point."
     def _insert_text(gate, pos, addition):
         properties = gate.cursor().text_properties()
@@ -55,31 +92,31 @@ class TextInsertionCommand(WriteCommand):
         # inserts the Region object into the target gate
         gate.insert_region(region, pos)
 
-class Insert_Character(TextInsertionCommand):
+class Insert_Character(TextInsertionCommand, NoExcursion):
     "Inserts the text character last entered into the target gate at point."
     @classmethod
-    def execute(cls, fie, config):
+    def body(cls, fie, config):
         string = fie.string
         if string[0:4] == "Shft":
             cls._insert_text(fie.gate, fie.gate.cursor().point(), string[5:])
         else:
             cls._insert_text(fie.gate, fie.gate.cursor().point(), string.lower())
 
-class Insert_Space(TextInsertionCommand):
+class Insert_Space(TextInsertionCommand, NoExcursion):
     "Inserts a space character into the target gate at point."
     @classmethod
-    def execute(cls, fie, config):
+    def body(cls, fie, config):
         cls._insert_text(fie.gate, fie.gate.cursor().point(), " ")
 
-class Insert_New_Line(TextInsertionCommand):
+class Insert_New_Line(TextInsertionCommand, NoExcursion):
     "Inserts a new line character into the target gate at point."
     @classmethod
-    def execute(cls, fie, config):
+    def body(cls, fie, config):
         cls._insert_text(fie.gate, fie.gate.cursor().point(), "\n")
         
-class Delete_Text(WriteCommand):
+class Delete_Text(WriteCommand, NoExcursion):
     "Deletes the selected text from the target gate.  If no text is selected, deletes the single character preceeding point."
-    def execute(fie, config):
+    def body(fie, config):
         gate = fie.gate
         cursor = gate.cursor()
         start = cursor.start()
@@ -91,19 +128,19 @@ class Delete_Text(WriteCommand):
             gate.selection(point-1, point, remove=True)
 
 ###### TEXT DISPLAY PROPERTY MANIPULATION ######
-class Portal_Map(NeutralCommand):
+class Portal_Map(NeutralCommand, ExcursionStart):
     "Sets the active keymap of the calling gate to 'Portal.'"
-    def execute(fie, config):
-        fie.gate.set_active_map(fie.commander.blueprints()["Keymaps"]["Portal"].materialize())
+    def body(fie, config):
+        fie.gate.set_keymap(fie.commander.blueprints()["Keymaps"]["Portal"].materialize())
 
-class Fonts_Map(NeutralCommand):
+class Fonts_Map(NeutralCommand, NoExcursion):
     "Sets the active keymap of the calling gate to 'Fonts.'"
-    def execute(fie, config):
-        fie.gate.set_active_map(fie.commander.blueprints()["Keymaps"]["Fonts"].materialize())
+    def body(fie, config):
+        fie.gate.set_keymap(fie.commander.blueprints()["Keymaps"]["Fonts"].materialize())
 
-class Toggle_Bold(WriteCommand):
+class Toggle_Bold(WriteCommand, ExcursionEnd):
     "Toggles the 'bold' property of the GateCursor from the target gate.  If there is a selection, toggles the 'bold' property of the selection as well."
-    def execute(fie, config):
+    def body(fie, config):
         gate = fie.gate
         cursor = gate.cursor()
         start = cursor.start()
@@ -121,11 +158,10 @@ class Toggle_Bold(WriteCommand):
             gate.insert_region(selection, start)
         else:
             cursor.set_text_property("bold", not is_bold)
-        gate.set_active_map(gate.primary_map())
 
-class Toggle_Italics(WriteCommand):
+class Toggle_Italics(WriteCommand, ExcursionEnd):
     "Toggles the 'italics' property of the GateCursor from the target gate.  If there is a selection, toggles the 'italics' property of the selection as well."
-    def execute(fie, config):
+    def body(fie, config):
         gate = fie.gate
         cursor = gate.cursor()
         start = cursor.start()
@@ -143,11 +179,10 @@ class Toggle_Italics(WriteCommand):
             gate.insert_region(selection, start)
         else:
             cursor.set_text_property("italics", not is_italics)
-        gate.set_active_map(gate.primary_map())
 
-class Toggle_Underline(WriteCommand):
+class Toggle_Underline(WriteCommand, ExcursionEnd):
     "Toggles the 'underline' property of the GateCursor from the target gate.  If there is a selection, toggles the 'underline' property of the selection as well."
-    def execute(fie, config):
+    def body(fie, config):
         gate = fie.gate
         cursor = gate.cursor()
         start = cursor.start()
@@ -165,41 +200,40 @@ class Toggle_Underline(WriteCommand):
             gate.insert_region(selection, start)
         else:
             cursor.set_text_property("underline", not is_underline)
-        gate.set_active_map(gate.primary_map())
 
 ###### NAVIGATION COMMANDS ######
 
 ## moving point with the mouse ##
 
-class Move_Point_To_Click(NeutralCommand):
+class Move_Point_To_Click(NeutralCommand, NoExcursion):
     "Moves point of the target gate to the location of the mouse click."
-    def execute(fie, config):
+    def body(fie, config):
         cursor = fie.gate.cursor()
         cursor.set_point(fie.gie.pos)
 
-class Move_Mark_To_Mouse_Location(NeutralCommand):
+class Move_Mark_To_Mouse_Location(NeutralCommand, NoExcursion):
     "Moves mark of the target gate to the current location of the mouse."
-    def execute(fie, config):
+    def body(fie, config):
         cursor = fie.gate.cursor()
         cursor.activate_mark()
         cursor.set_mark(fie.gie.pos)
 
-class Advance_Point_By_Char(NeutralCommand):
+class Advance_Point_By_Char(NeutralCommand, NoExcursion):
     "Moves point forwards by one character."
-    def execute(fie, config):
+    def body(fie, config):
         cursor = fie.gate.cursor()
         pos = cursor.point() + 1
         cursor.set_point(pos)
 
-class Retreat_Point_By_Char(NeutralCommand):
+class Retreat_Point_By_Char(NeutralCommand, NoExcursion):
     "Moves point backwards by one character."
-    def execute(fie, config):
+    def body(fie, config):
         cursor = fie.gate.cursor()
         pos = cursor.point() - 1
         cursor.set_point(pos)
 
 ## moving point by regex ##        
-class PointMatchCommand(NeutralCommand):
+class PointMatchCommand(NeutralCommand, NoExcursion):
     "Base class for commands that involve moving point to a location in the target gate that matches a regex."
     def _search_string_forwards(pattern, string):
         import re
@@ -238,38 +272,38 @@ class PointMatchCommand(NeutralCommand):
         pos = cls._search_string_backwards(pattern, remaining)
         cursor.set_point(pos)
 
-class Advance_Point_By_Word(PointMatchCommand):
+class Advance_Point_By_Word(PointMatchCommand, NoExcursion):
     "Moves point to the start of the next word in the target gate."
     @classmethod
-    def execute(cls, fie, config):
+    def body(cls, fie, config):
         cls._advance_point_to_match(fie.gate, "\s")
         cls._advance_point_to_match(fie.gate, "[^\s]")
 
-class Retreat_Point_By_Word(PointMatchCommand):
+class Retreat_Point_By_Word(PointMatchCommand, NoExcursion):
     "Moves point to the start of the previous word in the target gate."
     @classmethod
-    def execute(cls, fie, config):
+    def body(cls, fie, config):
         cls._retreat_point_to_match(fie.gate, "[^\s]")
         cls._retreat_point_to_match(fie.gate, "\s")
         cls._advance_point_to_match(fie.gate, "[^\s]")
 
-class Advance_Point_By_Sentence(PointMatchCommand):
+class Advance_Point_By_Sentence(PointMatchCommand, NoExcursion):
     "Moves point to the start of the next sentence in the target gate."
     @classmethod
-    def execute(cls, fie, config):
+    def body(cls, fie, config):
         cls._advance_point_to_match(fie.gate, "\.\s")
         cls._advance_point_to_match(fie.gate, "[^\.\s]")
 
-class Retreat_Point_By_Sentence(PointMatchCommand):
+class Retreat_Point_By_Sentence(PointMatchCommand, NoExcursion):
     "Moves point to the start of the previous sentence in the target gate."
     @classmethod
-    def execute(cls, fie, config):
+    def body(cls, fie, config):
         cls._retreat_point_to_match(fie.gate, "[^\.\s]")
         cls._retreat_point_to_match(fie.gate, "\.\s")
         cls._advance_point_to_match(fie.gate, "[^\.\s]")
 
 ## moving point by line divisions in window
-class PointWindowLineCommand(NeutralCommand):
+class PointWindowLineCommand(NeutralCommand, NoExcursion):
     "Base class of commands that move point relative to visual lines in the window."
 
     def _start_of_line(gate, gte):
@@ -313,29 +347,29 @@ class PointWindowLineCommand(NeutralCommand):
             current_layout_loc = (block.blockNumber(), line.lineNumber())
         return pos-1
 
-class Move_Point_Start_Of_Line(PointWindowLineCommand):
+class Move_Point_Start_Of_Line(PointWindowLineCommand, NoExcursion):
     "Moves point to the start of the current visual line."
     @classmethod
-    def execute(cls, fie, config):
+    def body(cls, fie, config):
         gate = fie.gate
         gte = fie.gie.win.gte()
         cursor = fie.gate.cursor()
         pos = cls._start_of_line(gate, gte)
         cursor.set_point(pos)
     
-class Move_Point_End_Of_Line(PointWindowLineCommand):
+class Move_Point_End_Of_Line(PointWindowLineCommand, NoExcursion):
     "Moves point to the end of the current visual line."
     @classmethod
-    def execute(cls, fie, config):
+    def body(cls, fie, config):
         gate = fie.gate
         gte = fie.gie.win.gte()
         cursor = fie.gate.cursor()
         pos = cls._end_of_line(gate, gte)
         cursor.set_point(pos)
 
-class Move_Point_Next_Line(PointWindowLineCommand):
+class Move_Point_Next_Line(PointWindowLineCommand, NoExcursion):
     "Moves point to the position in the next visual line with the same column number. Will try to preserve the column number if navigation is interrupted by a single empty or whitespace line."
-    def execute(fie, config):
+    def body(fie, config):
         gate = fie.gate
         gte = fie.gie.win.gte()
         cursor = gate.cursor()
@@ -377,9 +411,9 @@ class Move_Point_Next_Line(PointWindowLineCommand):
         else:
             Move_Point_End_Of_Line.execute(fie, config)
 
-class Move_Point_Previous_Line(PointWindowLineCommand):
+class Move_Point_Previous_Line(PointWindowLineCommand, NoExcursion):
     "Moves point to the position in the next visual line with the same column number. Will try to preserve the column number if navigation is interrupted by a single empty or whitespace line."
-    def execute(fie, config):
+    def body(fie, config):
         gte = fie.gie.win.gte()
         cursor = fie.gate.cursor()
         point = cursor.point()
@@ -424,9 +458,9 @@ class Move_Point_Previous_Line(PointWindowLineCommand):
 
 ###### MARK, KILL, YANK ######            
           
-class Set_Mark(NeutralCommand):
+class Set_Mark(NeutralCommand, NoExcursion):
     "Toggles the status of mark in the target gate."
-    def execute(fie, config):
+    def body(fie, config):
         cursor = fie.gate.cursor()
         point = cursor.point()
         mark = cursor.mark()
@@ -439,9 +473,9 @@ class Set_Mark(NeutralCommand):
             cursor.set_mark(point)
             cursor.deactivate_mark()
 
-class Kill_Region(WriteCommand):
+class Kill_Region(WriteCommand, NoExcursion):
     "Kills the selected region."
-    def execute(fie, config):
+    def body(fie, config):
         gate = fie.gate
         cursor = gate.cursor()
         commander = fie.commander
@@ -451,9 +485,9 @@ class Kill_Region(WriteCommand):
         selection = gate.selection(start, end, remove=True)
         ring.add(selection)
 
-class Yank(WriteCommand):
+class Yank(WriteCommand, ExcursionStart):
     "Yanks from the kill ring."
-    def execute(fie, config):
+    def body(fie, config):
         gate = fie.gate
         cursor = gate.cursor()
         commander = fie.commander
@@ -462,11 +496,12 @@ class Yank(WriteCommand):
         attempt = ring.get()
         if attempt:
             gate.insert_region(attempt, point)
-            gate.set_active_map(fie.commander.blueprints()["Keymaps"]["Yank"].materialize())
+            config.excursions[gate.name()+"_Yank"] = Excursion(gate)
+            gate.set_keymap(fie.commander.blueprints()["Keymaps"]["Yank"].materialize())
     
-class Yank_Next(WriteCommand):
+class Yank_Next(WriteCommand, NoExcursion):
     "Replaces the current yank attempt with the next member of the kill ring."
-    def execute(fie, config):
+    def body(fie, config):
         gate = fie.gate
         cursor = gate.cursor()
         commander = fie.commander
@@ -483,9 +518,9 @@ class Yank_Next(WriteCommand):
         attempt = ring.get()
         gate.insert_region(attempt, point)
 
-class Yank_Previous(WriteCommand):
+class Yank_Previous(WriteCommand, NoExcursion):
     "Replaces the current yank attempt with the previous member of the kill ring."
-    def execute(fie, config):
+    def body(fie, config):
         gate = fie.gate
         cursor = gate.cursor()
         commander = fie.commander
@@ -501,24 +536,20 @@ class Yank_Previous(WriteCommand):
         attempt = ring.get()
         gate.insert_region(attempt, point)
 
-class Yank_Pop(WriteCommand):
+class Yank_Pop(WriteCommand, ExcursionEnd):
     "Confirm placement of current yank attempt while removing it from the kill ring."
-    def execute(fie, config):
-        gate = fie.gate
-        gate.set_active_map(gate.primary_map())
+    def body(fie, config):
         ring = fie.commander.ring()
         ring.remove(ring.index())
-        print(ring._members)
 
-class Yank_Place(WriteCommand):
+class Yank_Place(WriteCommand, ExcursionEnd):
     "Confirm placement of current yank attempt, allowing it to remain in the kill ring."
-    def execute(fie, config):
-        gate = fie.gate
-        gate.set_active_map(gate.primary_map())
+    def body(fie, config):
+        pass
 
-class Yank_Cancel(WriteCommand):
+class Yank_Cancel(WriteCommand, ExcursionEnd):
     "Cancel the yanking process, removing any attempts that are still in place."
-    def execute(fie, config):
+    def body(fie, config):
         gate = fie.gate
         cursor = gate.cursor()
         commander = fie.commander
@@ -528,8 +559,6 @@ class Yank_Cancel(WriteCommand):
         current = ring.get()
         start_of_current = point - current.length()
         gate.selection(start_of_current, point, remove=True)
-        # resets keymap
-        gate.set_active_map(gate.primary_map())
 
 ###### IMPORTS ######        
 from Control.Interfaces import Fragment, Region
